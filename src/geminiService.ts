cecb49a5b3b3d88115afe0ce54844930e51f3916
komgoma <daynadayna3061@gmail.com>
2025-07-29 12:38:48 +0900

    first commit

---
+++ src/geminiService.ts
@@ -0,0 +1,23 @@
+# Logs
+logs
+*.log
+dist
+dist-ssr
+*.local
+
+# Environment variables
+.env
+.env.local
+.env.*.local
+
+# Editor directories and files
+.vscode/*
+!.vscode/extensions.json
+.idea
+.DS_Store
+*.suo
+*.ntvs*
+*.njsproj
+*.sln
+*.sw?
+
